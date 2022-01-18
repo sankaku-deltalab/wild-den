f4326e1eca3e4a1bcfb540e41db59dd3cab59e4f
Sankaku <sankaku_dlt.45631@outlook.jp>
2022-01-18 23:13:50 +0900

    Add UpdateBookAsRead use-case

--- src/use-cases/index.ts
+++ src/use-cases/index.ts
@@ -3,3 +3,4 @@ export * from "./delete-book-thumbnail-cache";
 export * from "./load-book-blob";
 export * from "./load-book-thumbnail";
 export * from "./scan-books";
+export * from "./update-book-as-read";
---
+++ src/use-cases/update-book-as-read.ts
@@ -0,0 +1,20 @@
+import { Result, Ok, Err } from "ts-results";
+import { BookProps, BookFileBlob, BookCacheRepository } from "../core";
+import { DateUtil } from "../util";
+
+export class UpdateBookAsRead {
+  constructor(private readonly date: DateUtil) {}
+
+  run(cache: BookCacheRepository, blob: BookFileBlob): void {
+    const props = cache.getBookProps(blob.id);
+    if (props.err) return;
+    const oldState = props.val.readState;
+    const newState = oldState === "new" ? "reading" : oldState;
+    const updatedProps: BookProps = {
+      ...props.val,
+      lastReadDate: this.date.now(),
+      readState: newState,
+    };
+    cache.setBookProps(updatedProps);
+  }
+}
