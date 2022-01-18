3fc3298d0fa8fef3e406ca2374d9f637852d862d
Sankaku <sankaku_dlt.45631@outlook.jp>
2022-01-18 22:37:23 +0900

    Add LoadBookBlob

--- src/use-cases/load-book-blob.ts
+++ src/use-cases/load-book-blob.ts
@@ -1,17 +1,24 @@
 import { Result, Ok, Err } from "ts-results";
-import {
-  BookIdStr,
-  BookFileBlob,
-  BookSource,
-  BookCacheRepository,
-} from "../core";
+import { BookId, BookFileBlob, BookSource, BookCacheRepository } from "../core";
 
 export class LoadBookBlob {
   async run(
     sources: BookSource[],
     cache: BookCacheRepository,
-    id: BookIdStr
-  ): Promise<Result<BookFileBlob, "offline">> {
-    return new Err("offline");
+    id: BookId
+  ): Promise<
+    Result<BookFileBlob, "offline" | "not exists" | "source not found">
+  > {
+    const cachedBlob = cache.getBlob(id);
+    if (cachedBlob.ok) return new Ok(cachedBlob.val);
+
+    const source = sources.find((s) => s.getSourceId() == id.source);
+    if (!source) return new Err("source not found");
+
+    const loadedBlob = await source.loadBlob(id.file);
+    if (loadedBlob.err) return loadedBlob;
+
+    cache.setBlob(loadedBlob.val);
+    return new Ok(loadedBlob.val);
   }
 }
