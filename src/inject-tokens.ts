56919a8b32482abfd69035ac575cacef8f72e987
Sankaku <sankaku_dlt.45631@outlook.jp>
2022-02-11 18:08:06 +0900

    Delete unused inject tokens

--- src/inject-tokens.ts
+++ src/inject-tokens.ts
@@ -1,11 +1,9 @@
 export const injectTokens = {
   LocalBookRepository: "LocalBookRepository",
   DateUtil: "DateUtil",
-  OneDriveBookSourcesFactoryRaw: "OneDriveBookSourcesFactoryRaw",
   MsGraphClientWrapperFactory: "MsGraphClientWrapperFactory",
   MsalInstanceRepository: "MsalInstanceRepository",
   MsGraphClientUtil: "MsGraphClientUtil",
-  OneDriveConfigRepository: "OneDriveConfigRepository",
   OnlineBookDataRepositoryFactory: "OnlineBookDataRepositoryFactory",
   BookSourceFactory: "BookSourceFactory",
   OneDriveBookSourceFactory: "OneDriveBookSourceFactory",
