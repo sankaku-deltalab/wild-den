1e06c8c44f5e48fbb466f7a1bab0153cd08a833d
Sankaku <sankaku_dlt.45631@outlook.jp>
2022-02-13 00:02:56 +0900

    Add babel config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  build: {
+    // https://qiita.com/hiroyukiwk/items/b283ef5312b289be6ce8
+    babel: {
+      presets({ isServer }, [preset, options]) {
+        options.loose = true;
+      },
+    },
+  },
 };
 
 module.exports = nextConfig;
