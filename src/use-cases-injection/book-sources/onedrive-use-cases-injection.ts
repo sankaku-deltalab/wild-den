8f91884a712c6284851b579890a51287c20ceb8c
Sankaku <sankaku_dlt.45631@outlook.jp>
2022-02-12 15:24:26 +0900

    Add GetMsalInstance use case

--- src/use-cases-injection/book-sources/onedrive-use-cases-injection.ts
+++ src/use-cases-injection/book-sources/onedrive-use-cases-injection.ts
@@ -3,6 +3,12 @@ import {
   LoginToOneDrive,
   LoginToOneDriveImpl,
 } from "../../use-cases/book-sources/one-drive";
+import {
+  GetMsalInstance,
+  GetMsalInstanceImpl,
+} from "../../use-cases/book-sources/one-drive/get-msal-instance";
 
+export const getMsalInstance: GetMsalInstance =
+  container.resolve(GetMsalInstanceImpl);
 export const loginToOneDrive: LoginToOneDrive =
   container.resolve(LoginToOneDriveImpl);
---
+++ src/use-cases/book-sources/one-drive/get-msal-instance.ts
@@ -0,0 +1,23 @@
+import { inject, injectable, singleton } from "tsyringe";
+import { AccountInfo } from "@azure/msal-browser";
+import type { FunctionClass } from "../../../function-class";
+import { injectTokens as it } from "../../../inject-tokens";
+import { MsalInstanceRepository } from "./interfaces/msal-instance-repository";
+import { MsalInstanceType } from ".";
+
+type GetMsalInstanceType = () => MsalInstanceType;
+
+export interface GetMsalInstance extends FunctionClass<GetMsalInstanceType> {}
+
+@singleton()
+@injectable()
+export class GetMsalInstanceImpl implements GetMsalInstance {
+  constructor(
+    @inject(it.MsalInstanceRepository)
+    private readonly msalRepo: MsalInstanceRepository
+  ) {}
+
+  run() {
+    return this.msalRepo.get();
+  }
+}
