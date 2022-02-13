75d58a5b5ab68e05173ea79041c810f83f8b02b0
Sankaku <sankaku_dlt.45631@outlook.jp>
2022-02-13 10:49:58 +0900

    Add MsalInstanceRepositoryImpl

---
+++ src/book-source/one-drive/msal-instance-repository-impl.ts
@@ -0,0 +1,15 @@
+import { injectable, singleton } from "tsyringe";
+import { PublicClientApplication } from "@azure/msal-browser";
+import { MsalInstanceType } from "../../use-cases/book-sources/one-drive";
+import { MsalInstanceRepository } from "../../use-cases/book-sources/one-drive/interfaces";
+import { msalConfig } from "./ms-graph-client/msal-config";
+
+@singleton()
+@injectable()
+export class MsalInstanceRepositoryImpl implements MsalInstanceRepository {
+  private readonly msalInstance = new PublicClientApplication(msalConfig);
+
+  get(): MsalInstanceType {
+    return this.msalInstance;
+  }
+}
