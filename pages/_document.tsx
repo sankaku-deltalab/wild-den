f23d68af81c69a96831436538c3eb97b51113e42
Sankaku <sankaku_dlt.45631@outlook.jp>
2022-02-15 22:38:42 +0900

    Fix _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,6 +7,7 @@ class Document extends NextDocument<Props> {
     return (
       <Html>
         <Head>
+          <link rel="manifest" href="manifest.json" />
           <meta name="application-name" content="Wild Den" />
           <meta name="apple-mobile-web-app-capable" content="yes" />
           <meta
@@ -17,6 +18,7 @@ class Document extends NextDocument<Props> {
           <meta name="description" content="eBook Manager and Reader" />
           <meta name="format-detection" content="telephone=no" />
           <meta name="mobile-web-app-capable" content="yes" />
+          <link rel="shortcut icon" href="/favicon.ico" />
         </Head>
         <body>
           <Main />
