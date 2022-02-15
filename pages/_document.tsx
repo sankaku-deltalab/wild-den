63cce5d4b420fa5922000a56b4a16cc818e2c610
Sankaku <sankaku_dlt.45631@outlook.jp>
2022-02-15 22:24:33 +0900

    Add files for pwa

---
+++ pages/_document.tsx
@@ -0,0 +1,30 @@
+import NextDocument, { Html, Head, Main, NextScript } from "next/document";
+
+type Props = {};
+
+class Document extends NextDocument<Props> {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <meta name="application-name" content="Wild Den" />
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <meta
+            name="apple-mobile-web-app-status-bar-style"
+            content="default"
+          />
+          <meta name="apple-mobile-web-app-title" content="Wild Den" />
+          <meta name="description" content="eBook Manager and Reader" />
+          <meta name="format-detection" content="telephone=no" />
+          <meta name="mobile-web-app-capable" content="yes" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default Document;
