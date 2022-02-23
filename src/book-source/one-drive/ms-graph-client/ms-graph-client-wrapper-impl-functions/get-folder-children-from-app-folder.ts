5e13ca436bb4ca22061a7c84fbbbfdb6131a421e
Sankaku <sankaku_dlt.45631@outlook.jp>
2022-02-23 18:49:10 +0900

    Fix getFolderChildrenFromAppFolder

--- src/book-source/one-drive/ms-graph-client/ms-graph-client-wrapper-impl-functions/get-folder-children-from-app-folder.ts
+++ src/book-source/one-drive/ms-graph-client/ms-graph-client-wrapper-impl-functions/get-folder-children-from-app-folder.ts
@@ -2,13 +2,21 @@ import { getFolderChildrenRaw } from ".";
 import { Result } from "../../../../results";
 import { OneDriveItemError } from "../../one-drive-error";
 import type { DriveItem, MsGraphClientType } from "../../types";
-import { appFolderItemByPathApi } from "./api-url";
+
+// https://docs.microsoft.com/en-us/onedrive/developer/rest-api/concepts/special-folders-appfolder?view=odsp-graph-online
+export const appFolderItemChildrenByPathApi = (
+  folders: string[],
+  fileName: string
+): string => {
+  const folderPath = folders.join("/");
+  return `/drive/special/approot:/${folderPath}/${fileName}:/children`;
+};
 
 export const getFolderChildrenFromAppFolder = async (
   client: MsGraphClientType,
   parentPath: string[],
   folderName: string
 ): Promise<Result<DriveItem[], OneDriveItemError>> => {
-  const api = appFolderItemByPathApi(parentPath, folderName);
+  const api = appFolderItemChildrenByPathApi(parentPath, folderName);
   return await getFolderChildrenRaw(client, api, () => true);
 };
