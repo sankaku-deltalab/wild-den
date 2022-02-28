1f80d8e1a94b0239c77cbf93071c2731575ef627
Sankaku <sankaku_dlt.45631@outlook.jp>
2022-02-28 18:15:34 +0900

    Move msal-config.ts

--- src/book-source/one-drive/msal-instance-repository-impl.ts
+++ src/book-source/one-drive/msal-instance-repository-impl.ts
@@ -2,7 +2,7 @@ import { injectable, singleton } from "tsyringe";
 import { PublicClientApplication } from "@azure/msal-browser";
 import { MsalInstanceType } from "../../use-cases/book-sources/one-drive";
 import { MsalInstanceRepository } from "../../use-cases/book-sources/one-drive/interfaces";
-import { msalConfig } from "./ms-graph-client/msal-config";
+import { msalConfig } from "./msal/msal-config";
 
 @singleton()
 @injectable()
