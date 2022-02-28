25b7ae64f76614382b879fbf68a79749c55f0cf9
Sankaku <sankaku_dlt.45631@outlook.jp>
2022-02-28 23:35:31 +0900

    Add MsGraphClientWrapperRestFactoryImpl

---
+++ src/cloud-storages/one-drive/driver/ms-graph-client/ms-graph-client-wrapper-factory-rest-impl.ts
@@ -0,0 +1,58 @@
+import { injectable, singleton } from "tsyringe";
+import { AccountInfo } from "@azure/msal-browser";
+import { err, Result, ok } from "../../../../results";
+import {
+  SourceId,
+  SourceIdStr,
+  sourceIdToStr,
+  sourceNotAvailableError,
+  SourceNotAvailableError,
+} from "../../../../core";
+import {
+  MsalInstanceType,
+  msGraphScopes,
+} from "../../../../use-cases/book-sources/one-drive";
+import { getMsGraphClient } from "./get-ms-graph-client";
+import { msalInstanceAccountToSourceId } from "../../util";
+import { MsGraphClientWrapperRestFactory } from "../../interfaces/ms-graph-client-wrapper-rest-factory";
+import { MsGraphClientWrapperRest } from "../../interfaces/ms-graph-client-wrapper-rest";
+import { MsalInstanceRepository } from "../../../../use-cases/book-sources/one-drive/interfaces";
+import { MsGraphClientWrapperRestImpl } from "../ms-graph-client-wrapper-rest-impl";
+
+@singleton()
+@injectable()
+export class MsGraphClientWrapperRestFactoryImpl
+  implements MsGraphClientWrapperRestFactory
+{
+  constructor(private readonly msalRepo: MsalInstanceRepository) {}
+
+  getClientWrappers(): Record<SourceIdStr, MsGraphClientWrapperRest> {
+    const msalInstance = this.msalRepo.get();
+    const accounts = this.msalRepo.get().getAllAccounts();
+    return Object.fromEntries(
+      accounts.map((a) => [
+        sourceIdToStr(msalInstanceAccountToSourceId(a)),
+        this.getClientWrapperRaw(a, msalInstance),
+      ])
+    );
+  }
+
+  getClientWrapper(
+    sourceId: SourceId
+  ): Result<MsGraphClientWrapperRest, SourceNotAvailableError> {
+    const wrappers = this.getClientWrappers();
+
+    const sidStr = sourceIdToStr(sourceId);
+    if (!(sidStr in wrappers)) return err(sourceNotAvailableError(sourceId));
+    return ok(wrappers[sidStr]);
+  }
+
+  private getClientWrapperRaw(
+    account: AccountInfo,
+    msalInstance: MsalInstanceType
+  ): MsGraphClientWrapperRest {
+    // Should I use cache?
+    const pureClient = getMsGraphClient(account, msGraphScopes, msalInstance);
+    return new MsGraphClientWrapperRestImpl(pureClient);
+  }
+}
